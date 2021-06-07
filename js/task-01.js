349057875c9375128b942d8005980c9e49eda0c3
Iryna Puchkova <irynapuchkova18@gmail.com>
2021-06-07 03:00:14 +0300

    task 1 and task 2

---
+++ js/task-01.js
@@ -0,0 +1,36 @@
+// Посчитает и выведет в консоль количество категорий в ul#categories,
+// то есть элементов li.item.
+// Получится 'В списке 3 категории.'.
+
+const categoryEl = document.querySelector('#categories');
+// console.log(category);
+const categoryItemsEl = categoryEl.children;
+// console.log(categoryItemsEl);
+const categoryItemsElAmount = categoryItemsEl.length;
+
+console.log(`'В списке ${categoryItemsElAmount} категории.'`);
+
+// Для каждого элемента li.item в списке ul#categories,
+// найдет и выведет в консоль текст заголовка элемента(тега h2)
+// и количество элементов в категории(всех вложенных в него элементов li).
+
+// Например для первой категории получится:
+// Категория: Животные
+// Количество элементов: 4
+const spreadCategoryItemsEl = [...categoryItemsEl];
+
+const getTitleAndItemsAmount = array => {
+  const messageEl = [];
+  let result;
+  
+  array.forEach(element => {
+    const titleEl = element.firstElementChild.textContent;
+    const itemsAmountEl = element.querySelectorAll('li').length;
+    messageEl.push(`Категория: ${titleEl}, Количество элементов: ${itemsAmountEl}`);
+    result = messageEl.join('; ');
+  })
+  
+  return result;
+};
+
+console.log(getTitleAndItemsAmount(spreadCategoryItemsEl));
--- js/task-02.js
+++ js/task-02.js
@@ -1,3 +1,10 @@
+// Напиши скрипт,
+// который для каждого элемента массива ingredients создаст отдельный li,
+  
+// после чего вставит все li за одну операцию в список ul.ingredients.
+// Для создания DOM - узлов используй document.createElement().
+
+
 const ingredients = [
   'Картошка',
   'Грибы',
@@ -6,3 +13,20 @@ const ingredients = [
   'Зелень',
   'Приправы',
 ];
+
+
+const ingredientsListEl = document.querySelector('#ingredients');
+
+const createIngredientsListEl = array => {
+  let ingredientItemEl;
+
+  array.forEach(element => {
+    ingredientItemEl = document.createElement('li');
+    ingredientItemEl.textContent = element;
+    ingredientsListEl.append(ingredientItemEl);
+    ingredientItemEl.style.listStyle = 'none';
+  })
+  return ingredientsListEl;
+}
+
+console.log(createIngredientsListEl(ingredients));
